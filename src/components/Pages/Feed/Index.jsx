e0481bb07c2daa8530ac9f734cf9e3b473cc1f3b
valentincarrichon <valentincarrichon@outlook.com>
2021-04-25 23:18:02 +0200

    fix modal, header

--- src/components/Pages/Feed/Index.jsx
+++ src/components/Pages/Feed/Index.jsx
@@ -103,7 +103,7 @@ export default function Home (props){
             {showModal ?
 
                 <div class="fixed z-10 inset-0 overflow-y-auto">
-                    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                    <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
                     <div class="fixed inset-0 transition-opacity">
                         <div onClick={() => setShowModal(false)}  class="absolute inset-0 bg-gray-500 opacity-75"></div>
--- src/components/composants/Header.jsx
+++ src/components/composants/Header.jsx
@@ -51,14 +51,7 @@ export default withRouter((props)=>{
                             </div>
                         </div>
                         <div className="hidden sm:ml-6 lg:flex sm:items-center">
-                            {isAdmin() &&
-                                <Link  to="/parametres" className="mx-2">
-                                    <svg className={(props.location.pathname === "/parametres" ? "text-primary " : "text-gray-500 ")+"h-5 w-5 "}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"> 
-                                        <circle cx="12" cy="12" r="3" />
-                                        <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" />
-                                    </svg>
-                                </Link>
-                            }
+                           
 
                             {/*<!-- Profile dropdown -->*/}
                             {isAuthenticated() ?
@@ -121,11 +114,7 @@ export default withRouter((props)=>{
                         <Link onClick={()=>setIsOpenMobile(!isOpenMobile)} to="/users" className={(props.location.pathname === "/users" ? "border-primary bg-fourth text-gray-900 " : "border-transparent ") +" mt-1 block pl-3 pr-4 py-2 border-l-4  text-base font-medium text-gray-600 hover:text-gray-700 hover:border-fourth transition duration-150 ease-in-out"}>
                             Utilisateurs
                         </Link>
-                        {isAdmin() && 
-                            <Link onClick={()=>setIsOpenMobile(!isOpenMobile)} to="/parametres" className={(props.location.pathname === "/parametres" ? "border-primary bg-fourth text-gray-900 " : "border-transparent ") +" mt-1 block pl-3 pr-4 py-2 border-l-4 text-base font-medium text-gray-600 hover:text-gray-700 hover:border-fourth transition duration-150 ease-in-out"}>
-                                Parametres
-                            </Link>
-                        }
+
                     </div>
                     <div className="pt-4 pb-3 border-b border-primary">
                         {isAuthenticated() ?
